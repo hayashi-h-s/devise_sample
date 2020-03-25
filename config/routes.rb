2e7623c58a4ae888cba5c62e61ece8fa83602e2b
hayamizhin <max9173627>
2020-03-25 18:53:46 +0900

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    # :sessions => 'users/sessions'
+  } 
+  
 
   resources :users, only: [:index, :show]
   resources :posts, only: [:index, :show, :create] do
